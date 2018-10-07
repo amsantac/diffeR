013cb981673e569d14f6aa7cbc405b0ddb021e6e
amsantac <amsantac@gmail.com>
2018-10-07 00:57:58 -0500

    modify package title and upload pdf manual

--- demo/diffMetrics.R
+++ demo/diffMetrics.R
@@ -1,8 +1,8 @@
-ref <- raster(system.file("external/reference.rst", package="diffeR"))
-comp <- raster(system.file("external/comparison.rst", package="diffeR"))
+ref <- raster(system.file("external/reference.rst", package = "diffeR"))
+comp <- raster(system.file("external/comparison.rst", package = "diffeR"))
 
 # Crosstab matrix, total difference, quantity difference and allocation difference at the map level:
-(ctmatCompRef <- crosstabm(comp, ref, percent=TRUE)) 
+(ctmatCompRef <- crosstabm(comp, ref, percent = TRUE)) 
 
 # 
 diffTablej(ctmatCompRef)
@@ -11,7 +11,7 @@ diffTablej(ctmatCompRef)
 exchangeDij(ctmatCompRef)
 
 # 
-(diffMR <- differenceMR(comp, ref, eval="original"))
+(diffMR <- differenceMR(comp, ref, eval = "original"))
 
 #
 overallComponentsPlot(comp, ref)

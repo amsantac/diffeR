f8d36d9b2b7efb37ff4f27062939e0aa67d2b714
amsantac <amsantac@gmail.com>
2018-10-14 21:03:19 -0500

    add colorValues argument to overallSourcesPlot

--- R/overallSourcesPlot.R
+++ R/overallSourcesPlot.R
@@ -1,4 +1,5 @@
-overallSourcesPlot <- function(comp = NULL, ref = NULL, ctmatrix = NULL, analysis = "error", units = NULL, population = NULL, fontSize = NULL){
+overallSourcesPlot <- function(comp = NULL, ref = NULL, ctmatrix = NULL, analysis = "error", units = NULL, population = NULL, 
+                               fontSize = NULL, colorValues = NULL){
   
   ylab <- ifelse(is.null(ctmatrix), "Difference Size (percentage of domain)", 
                  ifelse(is.null(units), "Difference Size (units)", paste0("Difference Size (", units, ")")))
@@ -18,7 +19,9 @@ overallSourcesPlot <- function(comp = NULL, ref = NULL, ctmatrix = NULL, analysi
   
   resT2 <- melt(resT, id.var = "Category", variable.name = "differenceType")
 
-  ggplot() + geom_bar(aes_(x = ~differenceType, y = ~value, fill = ~Category), data = resT2, stat = "identity") + 
+  g <- ggplot() + geom_bar(aes_(x = ~differenceType, y = ~value, fill = ~Category), data = resT2, stat = "identity") + 
     labs(x = "Type of Difference", y = ylab, fill = '') + theme_classic() + scale_y_continuous(expand = c(0, 0)) +
     theme(text = element_text(size = fontSize))
+  if(!is.null(colorValues)) g <-  g + scale_fill_manual(values = colorValues)
+  return(g)
 }
